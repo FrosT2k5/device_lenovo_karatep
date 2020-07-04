f4437d5b43131ea29942b59fb032bd104fc24715
ypatil <androygaming@gmail.com>
2020-07-04 18:42:29 +0530

    Adding Board config

---
+++ BoardConfig.mk
@@ -0,0 +1,230 @@
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+-include vendor/lenovo/karatep/BoardConfigVendor.mk
+
+DEVICE_PATH := device/lenovo/karatep
+
+BOARD_VENDOR := lenovo-qcom
+
+#TODO: What is this:
+#TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
+
+# Enable dexpreopt to speed boot time
+#WITH_DEXPREOPT := true
+#WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+#DONT_DEXPREOPT_PREBUILTS := true
+#WITH_DEXPREOPT_PIC := true
+
+# Same board so might work
+# Platform
+TARGET_BOARD_PLATFORM := msm8937
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno505
+TARGET_CPU_CORTEX_A53 := true
+
+#should work
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8937
+TARGET_NO_BOOTLOADER := true
+
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a53
+
+
+# Kernel
+#BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237
+#BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78B0000
+#BOARD_KERNEL_CMDLINE += vmalloc=400M
+BOARD_KERNEL_CMDLINE := console=tty60,115200,n8 androidboot.console=tty60 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 
+BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 lpm_levels.sleep_disabled=1 gpt androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78B0000
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_LZ4C_DT := true
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+TARGET_KERNEL_ARCH := arm
+TARGET_KERNEL_CONFIG := karatep_defconfig
+TARGET_KERNEL_SOURCE := kernel/lenovo/msm8937
+TARGET_KERNEL_APPEND_DTB := true
+
+#TODO:
+# Audio
+#AUDIO_FEATURE_ENABLED_ALAC_OFFLOAD := true
+#AUDIO_FEATURE_ENABLED_APE_OFFLOAD := true
+#AUDIO_FEATURE_ENABLED_ACDB_LICENSE := true
+#AUDIO_FEATURE_ENABLED_AUDIOSPHERE := true
+#AUDIO_FEATURE_ENABLED_ANC_HEADSET := true
+#AUDIO_FEATURE_ENABLED_COMPRESS_VOIP := true
+#AUDIO_FEATURE_ENABLED_DEV_ARBI := true
+#AUDIO_FEATURE_ENABLED_EXT_HDMI := true
+#AUDIO_FEATURE_ENABLED_EXTN_FLAC_DECODER := true
+#AUDIO_FEATURE_ENABLED_EXTN_FORMATS := true
+#AUDIO_FEATURE_ENABLED_FLAC_OFFLOAD := true
+#AUDIO_FEATURE_ENABLED_FLUENCE := true
+#AUDIO_FEATURE_ENABLED_HFP := true
+#AUDIO_FEATURE_ENABLED_KPI_OPTIMIZE := true
+#AUDIO_FEATURE_ENABLED_HIFI_AUDIO := true
+#AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
+#AUDIO_FEATURE_ENABLED_PCM_OFFLOAD := true
+#AUDIO_FEATURE_ENABLED_PCM_OFFLOAD_24 := true
+#AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
+#AUDIO_FEATURE_ENABLED_SOURCE_TRACKING := true
+#AUDIO_FEATURE_ENABLED_SSR := true
+#AUDIO_FEATURE_ENABLED_VBAT_MONITOR := true
+#AUDIO_FEATURE_ENABLED_VORBIS_OFFLOAD := true
+#AUDIO_FEATURE_ENABLED_WMA_OFFLOAD := true
+#AUDIO_USE_LL_AS_PRIMARY_OUTPUT := true
+#AUDIO_FEATURE_ENABLED_SND_MONITOR := true
+AUDIO_FEATURE_ENABLED_COMPRESS_VOIP := true
+AUDIO_FEATURE_ENABLED_EXTN_FORMATS := true
+AUDIO_FEATURE_ENABLED_FLAC_OFFLOAD := true
+AUDIO_FEATURE_ENABLED_FLUENCE := true
+AUDIO_FEATURE_ENABLED_HFP := true
+AUDIO_FEATURE_ENABLED_KPI_OPTIMIZE := true
+AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
+AUDIO_FEATURE_ENABLED_PCM_OFFLOAD := true
+AUDIO_FEATURE_ENABLED_PCM_OFFLOAD_24 := true
+BOARD_USES_ALSA_AUDIO := true
+BOARD_USES_GENERIC_AUDIO := true
+TARGET_USES_QCOM_MM_AUDIO := true
+
+#TODO:
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+BLUETOOTH_HCI_USE_MCT := true
+
+#TODO:
+# Camera
+USE_DEVICE_SPECIFIC_CAMERA := true
+TARGET_CAMERASERVICE_CLOSES_NATIVE_HANDLES := true
+
+#TODO:
+# Charger
+BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_NO_CHARGER_LED := true
+
+#TODO:
+# CMHW
+BOARD_USES_CYANOGEN_HARDWARE := true
+BOARD_HARDWARE_CLASS += hardware/cyanogen/cmhw
+BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/cmhw
+
+#TODO:
+# CNE / DPM
+BOARD_USES_QCNE := true
+
+#TODO:
+# CPUsets
+ENABLE_CPUSETS := true
+
+#TODO:
+# Crypto
+TARGET_HW_DISK_ENCRYPTION := true
+
+#TODO:
+# Display
+BOART_USES_ADRENO := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+OVERRIDE_RS_DRIVER:= libRSDriver_adreno.so
+TARGET_USES_C2D_COMPOSITION := true
+TARGET_USES_ION := true
+USE_OPENGL_RENDERER := true
+## --- NOT SURE ABOUT THESE ------ ###
+#TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
+#SF_VSYNC_EVENT_PHASE_OFFSET_NS := 2000000
+#VSYNC_EVENT_PHASE_OFFSET_NS := 6000000
+#MAX_EGL_CACHE_KEY_SIZE := 12*1024
+#MAX_EGL_CACHE_SIZE := 2048*1024
+
+# Filesystem
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 67108864
+BOARD_CACHEIMAGE_PARTITION_SIZE    := 268435456
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 3758096384
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 26302463488
+
+#TODO:
+# FM
+BOARD_HAVE_QCOM_FM := true
+
+#TODO:
+# Init
+TARGET_INIT_VENDOR_LIB := libinit_montana
+TARGET_RECOVERY_DEVICE_MODULES := libinit_montana
+
+#TODO:
+# Keymaster
+TARGET_PROVIDES_KEYMASTER := true
+
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
+# Media
+TARGET_USES_MEDIA_EXTENSIONS := true
+
+# NFC
+NXP_CHIP_TYPE := 2
+
+# Peripheral manager
+TARGET_PER_MGR_ENABLED := true
+
+# Power
+TARGET_POWERHAL_VARIANT := qcom
+
+# Properties
+TARGET_SYSTEM_PROP += device/lenovo/karatep/system.prop
+
+# Qualcomm support
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QC_TIME_SERVICES := true
+
+#TODO:
+# Recovery
+BOARD_HAS_NO_SELECT_BUTTON := true
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Release Tools
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
+
+# SELinux
+include device/qcom/sepolicy/sepolicy.mk
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+
+# Sensors
+USE_SENSOR_MULTI_HAL := true
+
+# Wifi
+BOARD_HAS_QCOM_WLAN := true
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME := "wlan"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
